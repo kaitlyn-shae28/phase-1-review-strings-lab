38bf8f0d86a2d28ad32ce301c32c94ed478ae83c
Kaitlyn Williamson <kaitlyn.shae28@gmail.com>
2023-08-28 16:15:29 -0400

    y

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "kaitlynshae28"
+
+const welcomeMessage = "Welcome to Flatbook, kaitlynshae28!"
+
+const excitedWelcomeMessage  = "WELCOME TO FLATBOOK, KAITLYNSHAE28!"
+
+const shortGreeting = "Welcome, k!"
